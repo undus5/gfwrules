145342c07d2dcde1d28e992d63631a5883e52b81
dodo <dodo@example.com>
2019-12-01 13:36:34 +0800

    middle

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 WORKING_DIR="$( cd -P "$( dirname "$BASH_SOURCE" )" > /dev/null 2>&1 && pwd -P )"
 cd $WORKING_DIR;
 
-ruby ./scripts/surge.rb;
-ruby ./scripts/clash.rb;
-ruby ./scripts/pacs.rb;
+ruby surge.rb;
+ruby clash.rb;
+ruby pacs.rb;
